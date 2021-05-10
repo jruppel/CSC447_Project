716dfd25d3498d7b36f0cb5b6cdd243a785d7f56
Stephanie Niemiec <37914160+Stephamoeba@users.noreply.github.com>
2021-05-10 08:13:49 -0500

    Update chariot.php

--- chariot.php
+++ chariot.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="UTF-8">
-<link rel="stylesheet" href="bootstrap.css">
-<style type="text/css">
-              body {
-                            font: 14px sans-serif;
-                            text-align: center;
-              }
-</style>
-<body>
-              <div>
-                            <img src="tarot/chariot.png">
-                            <h2>The chariot stands before you.</h2>
-                            <hr>
-                            <br>
-                            <h3>" This leads naught but to a dead end. Return to the deck and try your luck once more. "</h3>
-                
+              <meta charset="UTF-8">
+		<title>Welcome</title>
+		<link rel="stylesheet" href="bootstrap.css">
+                            <style type="text/css">
+			body
+			{
+				font: 14px sans-serif;
+				text-align: center;
+			}
+		</style>
+	<body>
+                            <div>
+                                          <img src="tarot/chariot.png">
+                                          <h2>
+                                                        The chariot stands before you.
+                                          </h2>
+                                          <hr>
+                                          <br>
+                                          <h3>"
+                                                        This leads naught but to a dead end.
+                                                        Return to the deck and try your luck once more.
+                                          "</h3>
+                          </div>
+              </body>
+</html>

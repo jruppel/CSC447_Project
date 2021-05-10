35d9aca6b36063d1b9ee8f8c125037f7b0da664d
Stephanie Niemiec <37914160+Stephamoeba@users.noreply.github.com>
2021-05-10 00:26:07 -0500

    Update chariot.php

--- chariot.php
+++ chariot.php
@@ -14,15 +14,10 @@
 <body>
               <div>
                             <img src="tarot/chariot.png">
-                            <h2>
-                                          The chariot stands before you.
-                            </h2>
+                            <h2>The chariot stands before you.</h2>
                             <hr>
                             <br>
-                            <h3>"
-                                          This leads naught but to a dead end.
-                                          Return to the deck and try your luck once more.
-                            "</h3>
+                            <h3>" This leads naught but to a dead end. Return to the deck and try your luck once more. "</h3>
               </div>
 </body>
 
